ab1d464c91a97d15522ab064fc7fbc101eff0ab8
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-14 00:20:34 +0800

    added stack for better visuals

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,13 @@
+import { Stack } from "@mui/material";
 import CustomTabs from "../components/CustomTabs";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <>
+    <Stack>
       <CustomTabs />
       <Outlet />
-    </>
+    </Stack>
   );
 };
 
