7259b68cc42b1be12b2e3058dd03354d201d91c3
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-13 23:54:20 +0800

    added route from react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import DogImage from "./components/DogImage";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
-  return <DogImage />;
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<DogImage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
