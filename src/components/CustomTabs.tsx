aef32c32eacab4650a23421e7a9d603419b83807
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-15 00:01:24 +0800

    refactor to custom tab

--- src/components/CustomTabs.tsx
+++ src/components/CustomTabs.tsx
@@ -1,21 +1,22 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function CenteredTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
   return (
-    <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
-      <Tabs value={value} onChange={handleChange} centered>
-        <Tab label="Random Dog Generation" />
-        <Tab label="Dog Breed Generation" />
-      </Tabs>
-    </Box>
+    <Stack
+      className="flex flex-row justify-center p-5 bg-yellow-800"
+      direction={"row"}
+      spacing={2}
+    >
+      <Link to={"/dog"} style={{ color: "white", textDecoration: "none" }}>
+        Random Dog Pictures
+      </Link>
+      <Link
+        to={"/specific-dog"}
+        style={{ color: "white", textDecoration: "none" }}
+      >
+        Specific Dog Pictures
+      </Link>
+    </Stack>
   );
 }
